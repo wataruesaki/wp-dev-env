7d6494a03c17847b3d04713330ef6f7395a0ad3a
Wataru Esaki <build.wataru@gmail.com>
2022-04-05 03:40:07 +0900

    feat: dev server

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,5 +3,6 @@
 const mix = require('laravel-mix')
 
 mix
-  .js('src/js/**/*.js', 'theme/assets/js')
   .postCss('src/app.css', 'theme/assets/css', [require('tailwindcss')])
+  .js('src/js/*.js', 'theme/assets/js')
+  .browserSync(require('./bs-config'))
